3228728593ec9e1829d6e1b9782e440be6277f4a
Ali <alihussien615@gmail.com>
2020-03-27 04:49:48 +0200

    .

--- src/controller/Home.java
+++ src/controller/Home.java
@@ -3,7 +3,8 @@ import gui.HomeFrame;
 import gui.PrintingFrame;
 import gui.SingleFrame;
 
-public class Home {
+public class Home 
+{
 
 	public static SingleFrame home = new SingleFrame();
 	public static int choice = 0;
